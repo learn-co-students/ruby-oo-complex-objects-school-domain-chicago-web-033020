0b2c7385690530d388846a62ecff142fca02be20
imperfectcastle <bart.sakowski@gmail.com>
2020-04-03 11:47:51 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+class School
+
+  attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    #or! how do i get or? ||!!
+    
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+  
+  def grade(num)
+    @roster[num]
+  end
+
+  def sort
+
+ 
+    @roster.each do |grade, people|
+      people.sort!
+    end
+    
+
+  end
+
+    
+
+
+end
