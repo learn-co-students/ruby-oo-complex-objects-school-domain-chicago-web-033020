fc44d79efdef7429b2f88c1c06b5bb4e4ae7e228
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-06 12:58:04 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+require 'pry'
+class School
+  attr_reader :name
+  attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  # def roster
+  #   # binding.pry
+  # end
+
+
+  def add_student(name, grade)
+    if @roster[grade]
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    new_hash = {}
+    @roster.each do |key, value|
+      new_hash[key] = value.sort
+    end
+    new_hash
+  end
+end
